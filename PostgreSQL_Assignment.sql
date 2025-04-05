346ccf7b1e13a65c7b7701cb46ea955979e3e421
Md Sakib al Hasan <mdsakibalhasanprogrammer1@gmail.com>
2025-04-05 15:35:27 +0600

    complted

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,3 +1,31 @@
+--  create books table 
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    author VARCHAR(200) NOT NULL,
+    price NUMERIC(10, 2) CHECK (price >= 0) NOT NULL,
+    stock INT CHECK (stock >= 0) NOT NULL,
+    published_year INT CHECK (published_year > 0) NOT NULL
+);
+
+-- create customers table
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    "name" VARCHAR(200) NOT NULL,
+    email VARCHAR(200) UNIQUE NOT NULL,
+    joined_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+ --create orders table
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+    book_id INT REFERENCES books(id) ON DELETE CASCADE,
+    quantity INT CHECK (quantity > 0) NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
 
 -- Find books that are out of stock.
 SELECT  title FROM books
@@ -12,7 +40,7 @@ Order BY  price DESC LIMIT 1;
 -- Find the total number of orders placed by each customer.
 SELECT customers.name, count(orders.customer_id) As total_orders  FROM orders
 JOIN customers On orders.customer_id = customers.id
-GROUP by "customers"."name"
+GROUP by customers."name"
 
 
 --  Calculate the total revenue generated from book sales.
